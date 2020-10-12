e277e7415f9cafd9e1fefae8a9c5d255aad1166d
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-10-12 19:54:19 +0500

    Updates

---
+++ precache-manifest.6a337f55165b95d7b127660f56d656f5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b2e75266b57a30d0f8bed79a4f68fe",
+    "url": "/task-01/index.html"
+  },
+  {
+    "revision": "57e59ff851723eb730bd",
+    "url": "/task-01/static/css/main.e683428f.chunk.css"
+  },
+  {
+    "revision": "82c9a9d33bdd3b09c333",
+    "url": "/task-01/static/js/2.99263071.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-01/static/js/2.99263071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e59ff851723eb730bd",
+    "url": "/task-01/static/js/main.a2b7a93d.chunk.js"
+  },
+  {
+    "revision": "ea2c70efec9f44ae14d0",
+    "url": "/task-01/static/js/runtime-main.21dc5853.js"
+  },
+  {
+    "revision": "b18c9a51d3a4701e1166244b26e433fd",
+    "url": "/task-01/static/media/1660-gaming-1-4012-249095-290519041917.b18c9a51.jpg"
+  },
+  {
+    "revision": "6f46400d81590d684815037f2b6e4aff",
+    "url": "/task-01/static/media/20d87417485084d29f2af1773b9aebfa.6f46400d.jpg"
+  },
+  {
+    "revision": "b4226a44cb390af4361c164b73caae3f",
+    "url": "/task-01/static/media/77217552.b4226a44.webp"
+  },
+  {
+    "revision": "a26d69fc1851df96deb5baef1a97ba92",
+    "url": "/task-01/static/media/ASUS-Strix-Surround-Sound-Gaming-Headset-680x546.a26d69fc.jpg"
+  },
+  {
+    "revision": "b446e86e93828cee82f3da712bf956f3",
+    "url": "/task-01/static/media/XBSS-mb.b446e86e.webp"
+  },
+  {
+    "revision": "c3fb6f3da565bd8e43bcd66ad4ea8e98",
+    "url": "/task-01/static/media/bancontact.c3fb6f3d.svg"
+  },
+  {
+    "revision": "3a4b262f03e7d39d9777136bdfde884b",
+    "url": "/task-01/static/media/bg-video.3a4b262f.mp4"
+  },
+  {
+    "revision": "37adadb5b739a52da876b144cbe57771",
+    "url": "/task-01/static/media/car1.37adadb5.png"
+  },
+  {
+    "revision": "714f06a3da64c114a200ed5bb1ade9a8",
+    "url": "/task-01/static/media/car2.714f06a3.png"
+  },
+  {
+    "revision": "73769c217e29292c73ac472a4aa2b784",
+    "url": "/task-01/static/media/car3.73769c21.png"
+  },
+  {
+    "revision": "7cd3e49ec0a56b96aaef14aa7e6fdf6d",
+    "url": "/task-01/static/media/car4.7cd3e49e.png"
+  },
+  {
+    "revision": "30a6cff5fb5d406e4eaa7061ba04b9ef",
+    "url": "/task-01/static/media/car5.30a6cff5.png"
+  },
+  {
+    "revision": "c80b9610e1ff4fcf2814c4a42c109127",
+    "url": "/task-01/static/media/england.c80b9610.png"
+  },
+  {
+    "revision": "8d00b726914278beec719c10f304c089",
+    "url": "/task-01/static/media/game1.8d00b726.jpg"
+  },
+  {
+    "revision": "634d155957cfee3fec4c2b5a1c0cf81e",
+    "url": "/task-01/static/media/game2.634d1559.jpg"
+  },
+  {
+    "revision": "6d50c989dacd3d81b4f8e22b55a8d436",
+    "url": "/task-01/static/media/logo.6d50c989.png"
+  },
+  {
+    "revision": "0814edab414f33b25506a5b6ad66f6e3",
+    "url": "/task-01/static/media/mastercard.0814edab.png"
+  },
+  {
+    "revision": "a37f8f2a88def62f863861bc10f718aa",
+    "url": "/task-01/static/media/puma-gaming-sock-hed-page-2019.a37f8f2a.png"
+  },
+  {
+    "revision": "b24e45eaf057e5fd5c0d6905cb80fb0f",
+    "url": "/task-01/static/media/visa.b24e45ea.svg"
+  }
+]);
